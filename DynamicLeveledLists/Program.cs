8f81682cb815bb4899d7cc26579c1cf5d49cd6f7
Justin Swanson <justin.c.swanson@gmail.com>
2018-10-24 22:12:42 -0500

    Initial starter project

---
+++ DynamicLeveledLists/Program.cs
@@ -0,0 +1,45 @@
+﻿using Mutagen.Bethesda;
+using Mutagen.Bethesda.Oblivion;
+using Mutagen.Bethesda.Oblivion.Internals;
+using Noggog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicLeveledLists
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await OblivionPipeline.TypicalPatch(
+                mainArgs: args,
+                outputMod: ModKey.Factory("Test.esp"),
+                importMask: new GroupMask(false)
+                {
+                    Creatures = true,
+                    NPCs = true,
+                    LeveledCreatures = true,
+                },
+                processor: CreateMod);
+        }
+
+        static async Task<OblivionMod> CreateMod(ModList<OblivionMod> modList)
+        {
+            var dynamicLeveledLists = new OblivionMod();
+            foreach (var mod in modList)
+            {
+                dynamicLeveledLists.AddRecords(
+                    mod.Mod,
+                    new GroupMask()
+                    {
+                        NPCs = true
+                    });
+            }
+            return dynamicLeveledLists;
+        }
+    }
+}
